2d73523b6e621cd66a6e394a60a4c82a8a67ab58
jkopen <jknoop@wice.de>
2020-12-16 11:25:45 +0100

    Removing empty arrays

--- test/transformations.test.js
+++ test/transformations.test.js
@@ -166,7 +166,7 @@ describe('Transformations', () => {
             ],
           },
         ],
-        organization: [],
+        // organization: [],
         picture: '',
         scope: 'PRIVATE',
       },
